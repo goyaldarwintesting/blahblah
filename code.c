9527cb771b64d10702f65b6e751c52f93f87b373
goyaldarwintesting <darwingoyal007testing@gmail.com>
2016-07-23 16:30:42 +0530

    created code.c
    this file was created by goyaldarwintesting

---
+++ code.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+  printf("This is the third file that i m adding on original repository and want to test whether i can pull the changes made in the original repository by all the people on my goyaldarwin account");
+
+}
